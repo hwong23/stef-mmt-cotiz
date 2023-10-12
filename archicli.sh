0d0cb5440cec8d0819427d6d0f673718ef9efd94
hwo <hwong23@gmail.com>
2023-10-11 21:58:21 -0500

    init

---
+++ archicli.sh
@@ -0,0 +1,3 @@
+#sh archicli.sh ~/git archigit/ markdownDrivingViews-devdocsCLI.ajs arqdoc
+
+/Applications/Archi.app/Contents/MacOS/Archi -application com.archimatetool.commandline.app -consoleLog -nosplash --modelrepository.loadModel $1/$2 --script.runScript $1/jarchi-hwo/hwo/$3 -vistaDocumental $4
